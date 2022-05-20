5c6a3652bac8cae8be68cfbc41303b1c8c5d8fa0
LoopFruits <Kensworldc@gmail.com>
2022-05-20 18:21:20 -0400

    amaking backend user auth routes

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+    has_many :todos
+
+    has_secure_password
+
+    #validations for user signup
+    validates :username, presence: true, uniqness: true
+    validates :password, length: { in: 5..15 }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'todos/index'
+  get 'todos/create'
+  get 'todos/update'
+  get 'todos/destroy'
+  get 'user/create'
+  get 'user/show'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
